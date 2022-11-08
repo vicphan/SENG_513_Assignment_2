895f843711bae1e8526d1e060c061087381e0652
Vic (Vu) Phan <65319838+vicphan@users.noreply.github.com>
2022-11-07 17:09:56 -0700

    made changes to router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
 );
