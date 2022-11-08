d0069c807e35670eb0f162c1981c2b24eff3b240
Vic (Vu) Phan <65319838+vicphan@users.noreply.github.com>
2022-11-07 17:17:23 -0700

    change BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <App />
     </Router>
 );
